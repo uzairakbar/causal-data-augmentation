ef92dbe9669cb9f5b8ae84b0a89e3cf931de66f8
Uzair Akbar <uzairakbar025@gmail.com>
2022-10-25 23:35:58 +0200

    Add linear DAIV regressors.

--- src/regressors/abstract.py
+++ src/regressors/abstract.py
@@ -27,3 +27,21 @@ class IVRegressor(Regressor):
     def fit(self, X, y, Z):
         pass
 
+
+class DAIVRegressor(IVRegressor, EmpiricalRiskMinimizer):
+    def __init__(self, alpha=1.0):
+        self._alpha = alpha
+        super(DAIVRegressor, self).__init__()
+    
+    @property
+    def alpha(self):
+        return self._alpha
+
+    @alpha.setter
+    def alpha(self, alpha):
+        self._alpha = alpha
+    
+    @abstractmethod
+    def fit(self, X, y, G, GX):
+        pass
+
