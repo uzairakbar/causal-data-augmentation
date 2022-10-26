7fcc839bdc488b252a7be0a0ef9366fde8516e9f
Uzair Akbar <uzairakbar025@gmail.com>
2022-10-26 02:43:49 +0200

    Add nonlinear simulation experiment.

---
+++ src/experiments/simulation/nonlinear.py
@@ -0,0 +1,135 @@
+import argparse
+import numpy as np
+
+from src.data_augmentors.simulation.nonlinear import NonlinearSimulationDA as DA
+
+from src.sem.simulation.nonlinear import NonlinearSimulationSEM as SEM
+
+from src.regressors.iv import IVGeneralizedMomentMethod as IV
+from src.regressors.erm import LeastSquaresGradientDescent as ERM
+from src.regressors.daiv import DAIVGeneralizedMomentMethod as DAIV
+
+from src.regressors.model_selectors import LeaveOneOut as LOO
+from src.regressors.model_selectors import LeaveOneLevelOut as LOLO
+
+from src.experiments.utils import (
+    set_seed,
+    grid_plot,
+    tex_table,
+)
+
+
+ALL_METHODS = {
+    "ERM": lambda: ERM(model="2-layer"),
+    "DA+ERM": lambda: ERM(model="2-layer"),
+    "DAIV+LOO": lambda: LOO(
+        estimator=DAIV(model="2-layer"),
+        param_distributions = {"alpha": np.random.lognormal(1, 1, 10)},
+        cv=5                                # TODO: proper LOO CV
+    ),
+    "DAIV+LOLO": lambda: LOLO(
+        estimator=DAIV(model="2-layer"),
+        param_distributions = {"alpha": np.random.lognormal(1, 1, 10)}
+    ),
+    "DA+IV": lambda: IV(model="2-layer")
+}
+
+
+def run_experiment(args):
+    if args["seed"] >= 0:
+        set_seed(args["seed"])
+    
+    if args["methods"] == "all":
+        methods = ALL_METHODS
+    else:
+        methods = {m: ALL_METHODS[m] for m in args["methods"].split(',')}
+    
+    all_sems = []
+    all_augmenters = []
+    for function in SEM.get_functions():
+        sem = SEM(function)
+        da = DA(function)
+        all_sems.append(sem)
+        all_augmenters.append(da)
+    
+    mse = lambda y, yhat: ((y - yhat)**2).mean()
+    all_functions = {
+        func: {
+            name: np.zeros((100, args["n_experiments"])) for name in methods
+        } for func in SEM.get_functions()
+    }
+    all_errors = {
+        func: {
+            name: np.zeros(args["n_experiments"]) for name in methods
+        } for func in SEM.get_functions()
+    }
+    x_gt = np.linspace(-5, 5, 100).reshape(-1, 1)
+    for j, (sem, da) in enumerate(zip(all_sems, all_augmenters)):
+        all_functions[sem.function_name]["x"] = x_gt.flatten()
+        all_functions[sem.function_name]["y"] = sem.f(x_gt).flatten()
+        for i in range(args["n_experiments"]):
+            print(f"{sem.function_name}: {i}")
+        
+            X, y = sem(N = args["n_samples"])
+            GX, G = da(X)
+
+            X_test, _ = sem(N = args["n_samples"])
+            y_test = sem.f(X_test)
+
+            if "x_data" in all_functions[sem.function_name]:
+                all_functions[sem.function_name]["x_data"] = np.append(
+                    all_functions[sem.function_name]["x_data"], X.flatten()
+                )
+                all_functions[sem.function_name]["y_data"] = np.append(
+                    all_functions[sem.function_name]["y_data"], y.flatten()
+                )
+            else:
+                all_functions[sem.function_name]["x_data"] = X.flatten()
+                all_functions[sem.function_name]["y_data"] = y.flatten()
+            
+
+            for method_name, method in methods.items():
+                model = method()
+                if "ERM" in method_name:
+                    if "DA" in method_name:
+                        model.fit(X=GX, y=y)
+                    else:
+                        model.fit(X=X, y=y)
+                elif "DAIV" in method_name:
+                    model.fit(X=X, y=y, G=G, GX=GX)
+                else:
+                    model.fit(X=GX, y=y, Z=G)
+
+                y_hat = model.predict(x_gt)
+                
+                all_functions[sem.function_name][method_name][:, i] = (
+                    y_hat.flatten()
+                )
+                
+                y_test_hat = model.predict(X_test)
+                all_errors[sem.function_name][method_name][i] = (
+                    mse(y_test, y_test_hat)
+                )
+
+    return all_functions, all_errors
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Nonlinear Simulation')
+    parser.add_argument('--n_samples', type=int, default=1000)
+    parser.add_argument('--n_experiments', type=int, default=10)
+    parser.add_argument('--seed', type=int, default=42)  # Negative is random
+    parser.add_argument('--methods', type=str, default="all")
+    args = dict(vars(parser.parse_args()))
+
+    all_functions, all_errors = run_experiment(args)
+    grid_plot(all_functions, fname="nonlinear_simulation")
+    tex_table(
+        all_errors, fname="nonlinear_simulation",
+        title="Test MSE $\\pm$ one standard deviation across $10$ runs."
+    )
+
+
+if __name__ == '__main__':
+    main()
+
--- src/experiments/utils.py
+++ src/experiments/utils.py
@@ -77,3 +77,135 @@ def sweep_plot(x, y,
         fname = "".join(c for c in xlabel if c.isalnum()) + "_sweep"
         fig.savefig(f"assets/{fname}.pdf", format="pdf", dpi=1200)
 
+
+def grid_plot(data, save=True, fname="nonlinear_simulation"):
+    label = {
+        "Data": "Data",
+        "ERM": "ERM",
+        "DA+ERM": "DA+ERM",
+        "DA+IV": "DA+IV",
+        "DAIV+LOO": r"DAIV-$\alpha^{\mathrm{\mathsf{LOO}}}$",
+        "DAIV+LOLO": r"DAIV-$\alpha^{\mathrm{\mathsf{LOLO}}}$",
+    }
+
+    functions = data.keys()
+    methods = ["Data"] + ([
+        method for method in data["abs"].keys() if "ERM" in method or "DA" in method or "IV" in method
+    ])
+    
+    sns.set_style("darkgrid")
+    colors = sns.color_palette()[:3]
+    fig, axs = plt.subplots(
+        len(functions), len(methods), figsize=(18, 9), sharex=True, sharey=False
+    )
+    for i, function in enumerate(functions):
+        for j, method in enumerate(methods):
+            x = data[function]["x"]
+            y = data[function]["y"]
+            
+            if method == "Data":
+                axs[i, j].scatter(
+                    data[function]["x_data"],
+                    data[function]["y_data"],
+                    color=colors[-1],
+                    alpha=0.4
+                )
+            else:
+                mean = data[function][method].mean(axis=1)
+                low = np.percentile(data[function][method], 2.5, axis=1)
+                high = np.percentile(data[function][method], 97.5, axis=1)
+
+                axs[i, j].plot(x, mean, color=colors[0])
+                axs[i, j].fill_between(x, low, high, color=colors[0], alpha=0.1)
+                
+            axs[i, j].plot(x, y, color=colors[1])
+            if not i:
+                axs[i, j].set_title(label[method])
+            if not j:
+                axs[i, j].set_ylabel(function)
+            if j:
+                axs[i, j].yaxis.set_ticklabels([])
+
+            y_range = max(y) - min(y)
+            y_pad = (y_range/2)*1.5
+            axs[i, j].set_xlim([min(x) - 0.333, max(x) + 0.333])
+            axs[i, j].set_ylim([min(y) - y_pad, max(y) + y_pad])
+    plt.tight_layout(pad = 0.333)
+    plt.show()
+    if save:
+        fig.savefig(
+            f"assets/{fname}.pdf", format="pdf", dpi=1200, bbox_inches="tight"
+        )
+
+
+def tex_table(data,
+              fname,
+              title,
+              decimals=3):
+    label = {
+        "ERM": "ERM",
+        "DA+ERM": "DA+ERM",
+        "DAIV+LOO": "DAIV--$\\alpha^{\\text{LOO}}$",
+        "DAIV+LOLO": "DAIV--$\\alpha^{\\text{LOLO}}$",
+        "DAIV+CC": "DAIV--$\\alpha^{\\text{CC}}$",
+        "DA+IV": "DA+IV",
+    }
+    
+    if "ERM" in data:
+        row_names = None
+        results = [np.round((np.mean(v), np.std(v)), decimals) for v in data.values()]
+        minimum = min(results, key = lambda v : v[0])[0]
+        column_names = [label[k] for k in data]
+    else:
+        row_names = list(data.keys())
+        results = {}
+        minimum = {}
+        for row in row_names:
+            columns = {col: data[row][col] for col in label.keys() if col in data[row]}
+            results[row] = [np.round((np.mean(v), np.std(v)), decimals) for v in columns.values()]
+            minimum[row] = min(results[row], key = lambda v : v[0])[0]
+        column_names = [label[k] for k in data[row_names[0]]]
+    
+    with open(f"assets/{fname}.tex", "w+") as f:
+        backreturn = "\\\\\n" + " "*8
+
+        num_columns = len(column_names) + int(row_names is not None)
+        columns_preamble = " ".join(["c"]*num_columns)
+
+        columns = " & ".join(column_names)
+        if row_names is not None:
+            columns = " & " + columns
+        
+        def row_content(row_data, minimum):
+            row = " & ".join([
+                f"${mean:.3f}\\pm {std:.3f}$" if mean > minimum
+                else ("$\\mathbf{ "+f"{mean:.3f}\\pm {std:.3f}"+" }$")
+                for (mean, std) in row_data
+            ])
+            return row
+        
+        if row_names is not None:
+            content = backreturn.join([
+                f"{row_name} & " + row_content(results[row_name], minimum[row_name])
+                for row_name in row_names
+            ])
+        else:
+            content = row_content(results, minimum)
+        
+        f.write(f"""
+                \\begin{{table}}[ht]
+                    \\caption{{
+                        {title}
+                    }}
+                    \\centering
+                    \\begin{{tabular}}{{@{{}}{columns_preamble}@{{}}}}
+                        \\toprule
+                        {columns} \\\\
+                        \\midrule
+                        {content}\\\\
+                        \\bottomrule
+                    \\end{{tabular}}
+                    \\label{{table:nonlin}}
+                \\end{{table}}
+                    """.strip())
+
