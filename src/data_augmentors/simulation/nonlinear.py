a12813ddb47b73fdd1692eb93bf3353cb20592c5
Uzair Akbar <uzairakbar025@gmail.com>
2022-10-26 02:40:44 +0200

    Define nonlinear simulation data augmentation

---
+++ src/data_augmentors/simulation/nonlinear.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from src.data_augmentors.abstract import DataAugmenter as DA
+
+
+class NonlinearSimulationDA(DA):
+    def __init__(self, function_name):
+        AUGMENTATION = {
+            "abs": lambda x, g: (g * x),
+            "sin": lambda x, g: (x + 2*np.pi*g),
+            "step": lambda x, g: (x * 10**(g+1)),
+        }
+        SAMPLER = {
+            "abs": lambda N: np.random.choice([-1, 1], size=(N, 1)),
+            "sin": lambda N: np.random.choice([-1, 0, 1], size=(N, 1)),
+            "step": lambda N: np.random.choice([-1, 0, 1], size=(N, 1)),
+        }
+        self.function_name = function_name
+        self._augment = AUGMENTATION[function_name]
+        self._sampler = SAMPLER[function_name]
+    
+    def augment(self, X):
+        N = len(X)
+        G = self._sampler(N)
+
+        GX = self._augment(X, G)
+        
+        return GX, G
+
