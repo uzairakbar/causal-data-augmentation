2d34ede7f58f4e52501f73dbf80bc3c746e09360
Uzair Akbar <uzairakbar025@gmail.com>
2022-11-16 12:04:22 +0100

    Update regressors for MNIST experiments

--- src/regressors/abstract.py
+++ src/regressors/abstract.py
@@ -6,8 +6,12 @@ from sklearn.model_selection._search import BaseSearchCV
 
 
 class Regressor(ABC, BaseEstimator):
+    def fit(self, X, y, **kwargs):
+        X = X.reshape(*X.shape[:1], -1)
+        return self._fit(X, y, **kwargs)
+    
     @abstractmethod
-    def fit(self, X, y):
+    def _fit(self, X, y):
         pass
     
     @property
@@ -15,7 +19,12 @@ class Regressor(ABC, BaseEstimator):
         return self._W
 
     def predict(self, X):
-        return X @ self._W
+        X = X.reshape(*X.shape[:1], -1)
+        return self._predict(X)
+    
+    @abstractmethod
+    def _predict(self, X):
+        pass
 
 
 class EmpiricalRiskMinimizer(Regressor):
@@ -23,12 +32,15 @@ class EmpiricalRiskMinimizer(Regressor):
 
 
 class IVRegressor(Regressor):
-    @abstractmethod
     def fit(self, X, y, Z):
+        return super(IVRegressor, self).fit(X=X, y=y, Z=Z)
+
+    @abstractmethod
+    def _fit(self, X, y, Z):
         pass
 
 
-class DAIVRegressor(IVRegressor, EmpiricalRiskMinimizer):
+class DAIVRegressor(Regressor):
     def __init__(self, alpha=1.0):
         self._alpha = alpha
         super(DAIVRegressor, self).__init__()
@@ -41,15 +53,29 @@ class DAIVRegressor(IVRegressor, EmpiricalRiskMinimizer):
     def alpha(self, alpha):
         self._alpha = alpha
     
-    @abstractmethod
     def fit(self, X, y, G, GX):
+        # TODO: change this to simpler version for debugging!
+        GX = GX.reshape(*GX.shape[:1], -1)
+        X = X.reshape(*X.shape[:1], -1)
+        # return self._fit(X, y, G, GX)
+        return super(DAIVRegressor, self).fit(X=X, y=y, G=G, GX=GX)
+    
+    @abstractmethod
+    def _fit(self, X, y, G, GX):
         pass
 
 
 class ModelSelector(ABC, BaseSearchCV):
-    def __init__(self, **kwargs):
-        r2 = make_scorer(self.r2)
-        super(ModelSelector, self).__init__(**kwargs, scoring=r2)
+    def __init__(self, metric="r2", **kwargs):
+        if metric == "r2":
+            scoring = make_scorer(self.r2)
+        elif metric == "accuracy":
+            scoring = make_scorer(self.accuracy)
+        elif metric == "mse":
+            scoring = make_scorer(self.negative_mse)
+        else:
+            raise ValueError("Wrong value for validation metric.")
+        super(ModelSelector, self).__init__(**kwargs, scoring=scoring)
     
     @property
     def alpha(self):
@@ -66,4 +92,14 @@ class ModelSelector(ABC, BaseSearchCV):
         ss_total = ss(y - y.mean())
         r2 = 1 - ss_residual/ss_total
         return r2
+    
+    @staticmethod
+    def accuracy(y, yhat, **kwargs):
+        return (y == yhat).mean()
+    
+    @staticmethod
+    def negative_mse(y, yhat, **kwargs):
+        return -1.0 * (
+            (y - yhat)**2
+        ).mean()
 
--- src/regressors/erm.py
+++ src/regressors/erm.py
@@ -7,18 +7,44 @@ from src.regressors.abstract import EmpiricalRiskMinimizer as ERM
 
 
 class LeastSquaresClosedForm(ERM):
-    def fit(self, X, y):
+    def _fit(self, X, y):
         self._W = np.linalg.pinv(X) @ y
         return self
+    
+    def _predict(self, X):
+        return X @ self._W
 
 
 class LeastSquaresGradientDescent(ERM):
     _models = {
-        "linear": lambda input_dim: torch.nn.Linear(input_dim, 1, bias=False),
+        "linear": lambda input_dim: torch.nn.Sequential(
+            torch.nn.Linear(input_dim, 1, bias=False)
+        ),
         "2-layer": lambda input_dim: torch.nn.Sequential(
             torch.nn.Linear(input_dim, 20),
             torch.nn.LeakyReLU(0.2),
             torch.nn.Linear(20, 1)
+        ),
+        "cmnist": lambda input_dim: torch.nn.Sequential(
+            torch.nn.Linear(input_dim, 256),
+            torch.nn.ReLU(True),
+            torch.nn.Linear(256, 256),
+            torch.nn.ReLU(True),
+            torch.nn.Linear(256, 1),
+            torch.nn.Sigmoid()
+        ),
+        "rmnist": lambda input_dim: torch.nn.Sequential(
+            torch.nn.Unflatten(1, torch.Size([1, 28, 28])),
+            torch.nn.Conv2d(1, 32, kernel_size=5, stride=1, bias=False),
+            torch.nn.BatchNorm2d(32), torch.nn.ReLU(), torch.nn.MaxPool2d(2, 2),
+            torch.nn.Conv2d(32, 64, kernel_size=5, stride=1, bias=False),
+            torch.nn.BatchNorm2d(64), torch.nn.ReLU(), torch.nn.MaxPool2d(2, 2),
+            torch.nn.Flatten(1, -1),
+            torch.nn.Linear(64 * 4 * 4, 128),
+            torch.nn.Dropout(0.5),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 10),
+            torch.nn.LogSoftmax(dim=1)
         ),
     }
 
@@ -40,6 +66,10 @@ class LeastSquaresGradientDescent(ERM):
             if torch.cuda.is_available():
                 X_b = X_b.cuda()
                 y_b = y_b.cuda()
+            elif torch.backends.mps.is_available():
+                X_b = X_b.to("mps")
+                y_b = y_b.to("mps")
+            
             loss_val = self._optimizer.step(lambda: self.loss(X_b, y_b))
             losses += [loss_val.data.cpu().numpy()]
         print("  train loss ", np.mean(losses))
@@ -47,20 +77,35 @@ class LeastSquaresGradientDescent(ERM):
     def fit_f_batch(self, X, y):
         _ = self._optimizer.step(lambda: self.loss(X, y))
 
-    def fit(self, X, y):
+    def _fit(self, X, y):
         n, m = X.shape
 
+        self.f = self.__model(m)
+        self.f.float()
+        self.f.train()
+        if torch.cuda.is_available():
+            self.f = self.f.cuda()
+        elif torch.backends.mps.is_available():
+            self.f = self.f.to("mps")
+
+        self._optimizer = torch.optim.Adam(self.f.parameters(), lr=0.01)
+
+        if isinstance(self.f[-1], torch.nn.LogSoftmax):
+            y = torch.tensor(y, dtype=torch.long)
+        else:
+            y = torch.tensor(y, dtype=torch.float)
         X = torch.tensor(X, dtype=torch.float)
-        y = torch.tensor(y, dtype=torch.float)
+        
         if torch.cuda.is_available():
             X = X.cuda()
             y = y.cuda()
-
-        self.f = self.__model(m)
-        if torch.cuda.is_available():
-            self.f = self.f.cuda()
-        self.f.float()
-        self._optimizer = torch.optim.Adam(self.f.parameters(), lr=0.01)
+            print("Using CUDA")
+        elif torch.backends.mps.is_available():
+            X = X.to("mps")
+            y = y.to("mps")
+            print("Using MPS")
+        else:
+            print("Using CPU")
 
         batch_mode = "mini" if n >= 1000 else "full"
         train = data_utils.DataLoader(data_utils.TensorDataset(X, y),
@@ -72,13 +117,25 @@ class LeastSquaresGradientDescent(ERM):
             else:
                 print("g epoch %d / %d" % (epoch + 1, self._epochs))
                 self.fit_f_minibatch(train)
+        self.f.eval()
         return self
 
-    def predict(self, X):
+    def _predict(self, X):
         X = torch.tensor(X, dtype=torch.float)
+        
         if torch.cuda.is_available():
             X = X.cuda()
-        return self.f(X).data.cpu().numpy()
+        elif torch.backends.mps.is_available():
+            X = X.to("mps")
+        
+        output = self.f(X).data.cpu().numpy()
+
+        if isinstance(self.f[-1], torch.nn.LogSoftmax):
+            output = output.argmax(axis=1)[:, None].astype(int)
+        elif isinstance(self.f[-1], torch.nn.Sigmoid):
+            output = (output > 0.5).astype(int)
+        
+        return output
     
     @property
     def solution(self):
@@ -98,6 +155,11 @@ class LeastSquaresGradientDescent(ERM):
     @staticmethod
     def _loss(X, y, f):
         y_hat = f(X)
-        loss = F.mse_loss(y, y_hat, reduction='sum')
+        if isinstance(f[-1], torch.nn.LogSoftmax):
+            loss = F.nll_loss(y_hat, y.flatten(), reduction='sum')
+        elif isinstance(f[-1], torch.nn.Sigmoid):
+            loss = F.binary_cross_entropy(y_hat, y, reduction='sum')
+        else:
+            loss = F.mse_loss(y_hat, y, reduction='sum')
         return loss
 
