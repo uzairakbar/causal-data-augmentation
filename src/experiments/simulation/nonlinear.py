c29695b5c185945f841ed6af91fdc7cf2bf4e8be
Uzair Akbar <uzairakbar025@gmail.com>
2024-08-31 13:34:37 -0400

    Refactor with tqdm, typing and command line args

--- src/experiments/simulation/nonlinear.py
+++ src/experiments/simulation/nonlinear.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+from tqdm import tqdm
 
 from src.data_augmentors.simulation.nonlinear import NonlinearSimulationDA as DA
 
@@ -14,6 +15,7 @@ from src.regressors.model_selectors import LeaveOneOut as LOO
 from src.regressors.model_selectors import LeaveOneLevelOut as LOLO
 
 from src.experiments.utils import (
+    save,
     set_seed,
     grid_plot,
     tex_table,
@@ -21,34 +23,39 @@ from src.experiments.utils import (
 
 
 ALL_METHODS = {
-    "ERM": lambda: ERM(model="2-layer"),
-    "DA+ERM": lambda: ERM(model="2-layer"),
-    "DAIV+LOO": lambda: LOO(
-        metric="mse",
-        estimator=DAIV(model="2-layer"),
-        param_distributions = {"alpha": np.random.lognormal(1, 1, 10)},
+    'ERM': lambda: ERM(model='2-layer'),
+    'DA+ERM': lambda: ERM(model='2-layer'),
+    'DAIV+LOO': lambda: LOO(
+        metric='mse',
+        estimator=DAIV(model='2-layer'),
+        param_distributions = {'alpha': np.random.lognormal(1, 1, 10)},
         cv=5,                                # TODO: proper LOO CV
         n_jobs=-1,
     ),
-    "DAIV+LOLO": lambda: LOLO(
-        metric="mse",
-        estimator=DAIV(model="2-layer"),
-        param_distributions = {"alpha": np.random.lognormal(1, 1, 10)},
+    'DAIV+LOLO': lambda: LOLO(
+        metric='mse',
+        estimator=DAIV(model='2-layer'),
+        param_distributions = {'alpha': np.random.lognormal(1, 1, 10)},
         n_jobs=-1,
     ),
-    "mmDAIV": lambda: mmDAIV(model="2-layer"),
-    "DA+IV": lambda: IV(model="2-layer")
+    'mmDAIV': lambda: mmDAIV(model='2-layer'),
+    'DA+IV': lambda: IV(model='2-layer')
 }
 
 
-def run_experiment(args):
-    if args["seed"] >= 0:
-        set_seed(args["seed"])
+def run(
+        seed: int,
+        n_samples: int,
+        n_experiments: int,
+        methods: str
+    ):
+    if seed >= 0:
+        set_seed(seed)
     
-    if args["methods"] == "all":
+    if methods == 'all':
         methods = ALL_METHODS
     else:
-        methods = {m: ALL_METHODS[m] for m in args["methods"].split(',')}
+        methods = {m: ALL_METHODS[m] for m in methods.split(',')}
     
     all_sems = []
     all_augmenters = []
@@ -61,47 +68,55 @@ def run_experiment(args):
     mse = lambda y, yhat: ((y - yhat)**2).mean()
     all_functions = {
         func: {
-            name: np.zeros((100, args["n_experiments"])) for name in methods
+            name: np.zeros((100, n_experiments)) for name in methods
         } for func in SEM.get_functions()
     }
     all_errors = {
         func: {
-            name: np.zeros(args["n_experiments"]) for name in methods
+            name: np.zeros(n_experiments) for name in methods
         } for func in SEM.get_functions()
     }
     x_gt = np.linspace(-5, 5, 100).reshape(-1, 1)
-    for j, (sem, da) in enumerate(zip(all_sems, all_augmenters)):
-        all_functions[sem.function_name]["x"] = x_gt.flatten()
-        all_functions[sem.function_name]["y"] = sem.f(x_gt).flatten()
-        for i in range(args["n_experiments"]):
-            print(f"{sem.function_name}: {i}")
+    for j, (sem, da) in enumerate(pbar_sem := tqdm(
+            zip(all_sems, all_augmenters), total=SEM.num_functions(), desc='SEMs'
+        )):
+        pbar_sem.set_description(f'SEM {sem.function_name}')
+
+        all_functions[sem.function_name]['x'] = x_gt.flatten()
+        all_functions[sem.function_name]['y'] = sem.f(x_gt).flatten()
+        for i in tqdm(
+            range(n_experiments), total=n_experiments, desc='Experiments'
+            ):
         
-            X, y = sem(N = args["n_samples"])
+            X, y = sem(N = n_samples)
             GX, G = da(X)
 
-            X_test, _ = sem(N = args["n_samples"])
+            X_test, _ = sem(N = n_samples)
             y_test = sem.f(X_test)
 
-            if "x_data" in all_functions[sem.function_name]:
-                all_functions[sem.function_name]["x_data"] = np.append(
-                    all_functions[sem.function_name]["x_data"], X.flatten()
+            if 'x_data' in all_functions[sem.function_name]:
+                all_functions[sem.function_name]['x_data'] = np.append(
+                    all_functions[sem.function_name]['x_data'], X.flatten()
                 )
-                all_functions[sem.function_name]["y_data"] = np.append(
-                    all_functions[sem.function_name]["y_data"], y.flatten()
+                all_functions[sem.function_name]['y_data'] = np.append(
+                    all_functions[sem.function_name]['y_data'], y.flatten()
                 )
             else:
-                all_functions[sem.function_name]["x_data"] = X.flatten()
-                all_functions[sem.function_name]["y_data"] = y.flatten()
+                all_functions[sem.function_name]['x_data'] = X.flatten()
+                all_functions[sem.function_name]['y_data'] = y.flatten()
             
+            for method_name, method in (pbar_methods := tqdm(
+                    methods.items(), total=len(methods), desc='Methods'
+                )):
+                pbar_methods.set_description(f'{method_name}')
 
-            for method_name, method in methods.items():
                 model = method()
-                if "ERM" in method_name:
-                    if "DA" in method_name:
+                if 'ERM' in method_name:
+                    if 'DA' in method_name:
                         model.fit(X=GX, y=y)
                     else:
                         model.fit(X=X, y=y)
-                elif "DAIV" in method_name:
+                elif 'DAIV' in method_name:
                     model.fit(X=GX, y=y, G=G, GX=GX)
                 else:
                     model.fit(X=GX, y=y, Z=G)
@@ -116,27 +131,32 @@ def run_experiment(args):
                 all_errors[sem.function_name][method_name][i] = (
                     mse(y_test, y_test_hat)
                 )
-
-    return all_functions, all_errors
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Nonlinear Simulation')
-    parser.add_argument('--n_samples', type=int, default=1000)
-    parser.add_argument('--n_experiments', type=int, default=10)
-    parser.add_argument('--seed', type=int, default=42)  # Negative is random
-    parser.add_argument('--methods', type=str, default="all")
-    args = dict(vars(parser.parse_args()))
-
-    all_functions, all_errors = run_experiment(args)
-
-    grid_plot(all_functions, fname="nonlinear_simulation")
+    
+    grid_plot(all_functions, fname='nonlinear_simulation')
     tex_table(
-        all_errors, fname="nonlinear_simulation",
-        title="Test MSE $\\pm$ one standard deviation across $10$ runs."
+        all_errors, fname='nonlinear_simulation',
+        title='Test MSE $\\pm$ one standard deviation across $10$ runs.'
     )
+    save(all_errors, 'nonlinear_sim_errors')
+    save(all_functions, 'nonlinear_sim_functions')
 
 
 if __name__ == '__main__':
-    main()
-
+    parser = argparse.ArgumentParser(
+        description='Nonlinear simulation experiment.'
+    )
+    parser.add_argument(
+        '--seed', type=int, default=42, help='Random seed for the experiment. Negative is random.'
+    )
+    parser.add_argument(
+        '--n_samples', type=int, default=1000, help='Number of samples per experiment.'
+    )
+    parser.add_argument('--n_experiments', type=int, default=10, help='Number of experiments.')
+    parser.add_argument(
+        '--methods',
+        type=str,
+        default='all',
+        help='Methods to use. Specify in comma-separated format -- "ERM,DA+ERM,DA+UIV,DA+IV". Default is "all".'
+    )
+    args = parser.parse_args()
+    run(**args)
