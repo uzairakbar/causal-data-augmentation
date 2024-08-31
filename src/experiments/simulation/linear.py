bfc804f3388bfcde5577d213871ec0042760d832
Uzair Akbar <uzairakbar025@gmail.com>
2024-08-31 12:17:49 -0400

    Refactor with tqdm, logging, typing and comand line args

--- src/experiments/simulation/linear.py
+++ src/experiments/simulation/linear.py
@@ -1,10 +1,15 @@
+import argparse
 import numpy as np
+from tqdm import tqdm
+from loguru import logger
 from abc import ABC, abstractmethod
+from typing import Dict, Callable, Optional
 
 from src.data_augmentors.simulation.linear import NullSpaceTranslation as DA
 
 from src.sem.simulation.linear import LinearSimulationSEM as SEM
 
+from src.regressors.abstract import Regressor, ModelSelector
 from src.regressors.daiv import DAIVLeastSquaresClosedForm as DAIValpha
 from src.regressors.erm import LeastSquaresClosedForm as ERM
 from src.regressors.iv import IVTwoStageLeastSquares as IV
@@ -15,6 +20,7 @@ from src.regressors.model_selectors import LeaveOneOut as LOO
 from src.regressors.model_selectors import ConfounderCorrection as CC
 
 from src.experiments.utils import (
+    save,
     set_seed,
     bootstrap,
     sweep_plot,
@@ -39,26 +45,28 @@ ALL_METHODS = {
 
 
 class Experiment(ABC):
-    def __init__(self,
-                 x_dimension = 30,
-                 n_samples = 5000,
-                 n_experiments = 25,
-                 seed = 42,
-                 methods = "all",
-                 sweep_samples = 25):
+    def __init__(
+            self,
+            seed: int,
+            n_samples: int,
+            x_dimension: int,
+            n_experiments: int,
+            sweep_samples: int,
+            methods: Dict[str, Callable[[Optional[str]], Regressor | ModelSelector]]
+        ):
         self.x_dimension = x_dimension
         self.n_samples = n_samples
         self.n_experiments = n_experiments
         self.seed = seed
         self.sweep_samples = sweep_samples
-        if methods == "all":
-            self.methods = ALL_METHODS
-        else:
-            self.methods = {m: ALL_METHODS[m] for m in methods.split(',')}
+        self.methods = methods
     
     @staticmethod
-    def fit(method_name, method, X, y, G, GX, param):
-        if method_name == "DAIV":
+    def fit(method_name: str,
+            method: Callable[[Optional[str]], Regressor | ModelSelector],
+            X, y, G, GX,
+            param: float) -> Regressor | ModelSelector:
+        if method_name == "DAIValpha":
             model = method(alpha = param)
         else:
             model = method()
@@ -76,7 +84,7 @@ class Experiment(ABC):
         return model
     
     @abstractmethod
-    def generate_dataset(self, sem, da, param):
+    def generate_dataset(self, sem: SEM, da: DA, param: float):
         pass
 
     @abstractmethod
@@ -85,14 +93,14 @@ class Experiment(ABC):
 
     def compute_result(self,
                sem_solution,
-               method_name,
-               method,
+               method_name: str,
+               method: Callable[[Optional[str]], Regressor | ModelSelector],
                X, y, G, GX,
-               param):
+               param: float) -> float:
         model = self.fit(
             method_name, method, X, y, G, GX, param
         )
-        error = relative_sq_error(sem_solution, model.solution)
+        error = relative_sq_error(sem_solution, model.solution)**0.5
         return error
 
     def run_experiment(self):
@@ -111,12 +119,18 @@ class Experiment(ABC):
         error_dim = (self.sweep_samples, self.n_experiments)
         results = {name: np.zeros(error_dim) for name in self.methods}
 
-        for i, param in enumerate(param_values):
-            for j, (sem, da) in enumerate(zip(all_sems, all_augmenters)):
+        for i, param in enumerate(tqdm(
+            param_values, total=self.sweep_samples, desc='Parameters'
+            )):
+            for j, (sem, da) in enumerate(tqdm(
+                zip(all_sems, all_augmenters), total=self.n_experiments, desc='Experiments'
+                )):
                 sem_solution = sem.solution
 
                 X, y, G, GX = self.generate_dataset(sem, da, param)
-                for method_name, method in self.methods.items():
+                for method_name, method in tqdm(
+                    self.methods.items(), total=len(self.methods), desc='Methods'
+                    ):
                     results[method_name][i][j] = self.compute_result(
                         sem_solution, method_name, method, X, y, G, GX, param
                     )
@@ -124,7 +138,7 @@ class Experiment(ABC):
 
 
 class LambdaSweep(Experiment):
-    def generate_dataset(self, sem, da, param):
+    def generate_dataset(self, sem: SEM, da: DA, param: float):
         X, y = sem(N = self.n_samples, lamda = param)
         GX, G = da(X)
         return X, y, G, GX
@@ -137,7 +151,7 @@ class LambdaSweep(Experiment):
 
 
 class GammaSweep(Experiment):
-    def generate_dataset(self, sem, da, param):
+    def generate_dataset(self, sem: SEM, da: DA, param: float):
         X, y = sem(N = self.n_samples)
         GX, G = da(X, gamma = param)
         return X, y, G, GX
@@ -153,11 +167,11 @@ class AlphaSweep(Experiment):
     def __init__(self,
                  **kwargs):
         super(AlphaSweep, self).__init__(**kwargs)
-        self.methods["DAIV"] = (
-            lambda alpha: DAIV(alpha = alpha)
+        self.methods["DAIValpha"] = (
+            lambda alpha: DAIValpha(alpha = alpha)
         )
 
-    def generate_dataset(self, sem, da, param):
+    def generate_dataset(self, sem: SEM, da: DA, param: float):
         X, y = sem(N = self.n_samples)
         GX, G = da(X)
         return X, y, G, GX
@@ -170,32 +184,75 @@ class AlphaSweep(Experiment):
     
     def compute_result(self,
                sem_solution,
-               method_name,
-               method,
+               method_name: str,
+               method: Callable[[Optional[str]], Regressor | ModelSelector],
                X, y, G, GX,
-               param):
+               param: float) -> float:
         model = self.fit(
             method_name, method, X, y, G, GX, param
         )
-        error = relative_sq_error(sem_solution, model.solution)
+        error = relative_sq_error(sem_solution, model.solution)**0.5
         if "DAIV+" in method_name:
             return model.alpha
         else:
             return error
 
 
-def main():
-    lambda_values, results = LambdaSweep().run_experiment()
+def run(
+        seed: int,
+        n_samples: int,
+        x_dimension: int,
+        n_experiments: int,
+        sweep_samples: int,
+        methods: str
+    ):
+    if methods == "all":
+        methods = ALL_METHODS
+    else:
+        methods = {m: ALL_METHODS[m] for m in methods.split(',')}
+    
+    # sweep over lambda parameter
+    logger.info('Sweeping over lambda parameters.')
+    lambda_values, results = LambdaSweep(
+        seed=seed,
+        x_dimension=x_dimension,
+        n_samples=n_samples,
+        n_experiments=n_experiments,
+        methods=methods,
+        sweep_samples=sweep_samples
+    ).run_experiment()
     sweep_plot(
         lambda_values, bootstrap(results), xlabel=r"$\lambda$", xscale="linear"
     )
+    save(lambda_values, "lambda_values.pkl")
+    save(results, "lambda_results.pkl")
 
-    gamma_values, results = GammaSweep().run_experiment()
+    # sweep over gamma parameter
+    logger.info('Sweeping over gamma parameters.')
+    gamma_values, results = GammaSweep(
+        seed=seed,
+        x_dimension=x_dimension,
+        n_samples=n_samples,
+        n_experiments=n_experiments,
+        methods=methods,
+        sweep_samples=sweep_samples
+    ).run_experiment()
     sweep_plot(
         gamma_values, bootstrap(results), xlabel=r"$\gamma$", xscale="log"
     )
+    save(gamma_values, "gamma_values.pkl")
+    save(results, "gamma_results.pkl")
 
-    alpha_values, results = AlphaSweep().run_experiment()
+    # sweep over alpha parameter
+    logger.info('Sweeping over alpha parameters.')
+    alpha_values, results = AlphaSweep(
+        seed=seed,
+        x_dimension=x_dimension,
+        n_samples=n_samples,
+        n_experiments=n_experiments,
+        methods=methods,
+        sweep_samples=sweep_samples
+    ).run_experiment()
     vertical_plots = ([
         method for method in ALL_METHODS.keys() if "DAIV+" in method
     ])
@@ -203,8 +260,21 @@ def main():
         alpha_values, bootstrap(results), xlabel=r"$\alpha$", xscale="log",
         vertical_plots=vertical_plots, trivial_solution=False
     )
+    save(alpha_values, "alpha_values.pkl")
+    save(results, "alpha_results.pkl")
 
 
 if __name__ == "__main__":
-    main()
-
+    parser = argparse.ArgumentParser(description='Linear simulation experiment.')
+    parser.add_argument('--seed', default=42, help='Random seed for the experiment.')
+    parser.add_argument('--n_samples', default=2000, help='Number of samples per experiment.')
+    parser.add_argument('--x_dimension', default=30, help='Dimension of treatment.')
+    parser.add_argument('--n_experiments', default=10, help='Number of experiments.')
+    parser.add_argument('--sweep_samples', default=10, help='Sweep resolution across lambda, alpha and gamma.')
+    parser.add_argument(
+        '--methods',
+        default='all',
+        help='Methods to use. Specify in comma-separated format -- "ERM,DA+ERM,DA+UIV,DA+IV". Default is "all".'
+    )
+    args = parser.parse_args()
+    run(**args)
