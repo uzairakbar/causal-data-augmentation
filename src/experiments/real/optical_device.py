64f73771d360fa88dddf82dab9eddabd3bb844f9
Uzair Akbar <uzairakbar025@gmail.com>
2024-08-31 13:09:56 -0400

    Refactor with tqdm, typing and command line args

--- src/experiments/real/optical_device.py
+++ src/experiments/real/optical_device.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+from tqdm import tqdm
 
 from src.data_augmentors.real.optical_device import OpticalDeviceDA as DA
 
@@ -20,6 +21,7 @@ from src.regressors.model_selectors import LeaveOneLevelOut as LOLO
 from src.regressors.model_selectors import ConfounderCorrection as CC
 
 from src.experiments.utils import (
+    save,
     set_seed,
     relative_sq_error,
     bootstrap,
@@ -29,35 +31,41 @@ from src.experiments.utils import (
 
 
 ALL_METHODS = {
-    "ERM": lambda: ERM(),
-    "DA+ERM": lambda: ERM(),
-    "DAIV+LOO": lambda: LOO(
+    'ERM': lambda: ERM(),
+    'DA+ERM': lambda: ERM(),
+    'DAIV+LOO': lambda: LOO(
         estimator=DAIValpha(),
-        param_distributions = {"alpha": np.random.lognormal(1, 1, 10)},
+        param_distributions = {'alpha': np.random.lognormal(1, 1, 10)},
         cv=5,                                # TODO: proper LOO CV
         n_jobs=-1,
     ),
-    # "DAIV+LOLO": lambda: LOLO(
+    # 'DAIV+LOLO': lambda: LOLO(
     #     estimator=DAIValpha(),
-    #     param_distributions = {"alpha": np.random.lognormal(1, 1, 10)},
+    #     param_distributions = {'alpha': np.random.lognormal(1, 1, 10)},
     #     n_jobs=-1,
     # ),
-    "DAIV+CC": lambda: CC(estimator=DAIValpha()),
-    # "mmDAIV": lambda: mmDAIV(),
-    # "DAIVP_": lambda: pDAIV(),
-    "DAIVpi": lambda: DAIVpi(),
-    "DAIV": lambda: DAIV(),
-    "DA+IV": lambda: IV()
+    'DAIV+CC': lambda: CC(estimator=DAIValpha()),
+    # 'mmDAIV': lambda: mmDAIV(),
+    # 'DAIVP_': lambda: pDAIV(),
+    'DAIVpi': lambda: DAIVpi(),
+    'DAIV': lambda: DAIV(),
+    'DA+IV': lambda: IV()
 }
 
-def run(args):
-    if args["seed"] >= 0:
-        set_seed(args["seed"])
+
+def run(
+        seed: int,
+        n_samples: int,
+        alpha: float,
+        methods: str
+    ):
+    if seed >= 0:
+        set_seed(seed)
     
-    if args["methods"] == "all":
+    if methods == 'all':
         methods = ALL_METHODS
     else:
-        methods = {m: ALL_METHODS[m] for m in args["methods"].split(',')}
+        methods = {m: ALL_METHODS[m] for m in methods.split(',')}
     
     all_sems = []
     all_augmenters = []
@@ -71,50 +79,60 @@ def run(args):
     error_dim = (SEM.num_experiments(),)
     all_errors = {name: np.zeros(error_dim) for name in methods}
     
-    for j, (sem, da) in enumerate(zip(all_sems, all_augmenters)):
+    for j, (sem, da) in enumerate(tqdm(
+            zip(all_sems, all_augmenters), total=SEM.num_experiments(), desc='Experiments'
+        )):
         sem_solution = sem.solution
 
-        X, y = sem(N = args["n_samples"])
+        X, y = sem(N = n_samples)
         GX, G = da(X)
-        for method_name, method in methods.items():
-            print(f"######### {j} {method_name} #########")
+        for method_name, method in (pbar_methods := tqdm(
+                methods.items(), total=len(methods), desc='Methods'
+            )):
+            pbar_methods.set_description(f'{method_name}')
+
             model = method()
-            if "ERM" in method_name:
-                if "DA" in method_name:
+            if 'ERM' in method_name:
+                if 'DA' in method_name:
                     model.fit(X=GX, y=y)
                 else:
                     model.fit(X=X, y=y)
-            elif "DAIV" in method_name:
+            elif 'DAIV' in method_name:
                 model.fit(X=X, y=y, G=G, GX=GX)
             else:
-                model.fit(X=GX, y=y, Z=G)
+                model.fit(X=X, y=y, Z=G)
             
             method_solution = model.solution
             
             error = relative_sq_error(sem_solution, method_solution)
 
             all_errors[method_name][j] = error
-
-    return all_errors
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Optical Device Dataset')
-    parser.add_argument('--n_samples', type=int, default=1000)
-    parser.add_argument('--seed', type=int, default=42)  # Negative is random
-    parser.add_argument('--methods', type=str, default="all")
-    parser.add_argument('--alpha', type=float, default=2.0)
-    args = dict(vars(parser.parse_args()))
-
-    all_errors = run(args)
-
-    box_plot(bootstrap(all_errors), fname="optical_device")
+    
+    errors_bootstrapped = bootstrap(all_errors)
+    box_plot(errors_bootstrapped, fname='optical_device')
     tex_table(
-        bootstrap(all_errors), fname="optical_device",
-        title="Results for the optical device experiment."
+        errors_bootstrapped, fname='optical_device',
+        title='Results for the optical device experiment.'
     )
+    save(all_errors, 'optical_device')
 
 
 if __name__ == '__main__':
-    main()
-
+    parser = argparse.ArgumentParser(
+        description='Optical device dataset experiment.'
+    )
+    parser.add_argument(
+        '--seed', type=int, default=42, help='Random seed for the experiment. Negative is random.'
+    )
+    parser.add_argument(
+        '--n_samples', type=int, default=1000, help='Number of samples per experiment.'
+    )
+    parser.add_argument('--alpha', type=float, default=2.0, help='UIValpha regressoin parameter.')
+    parser.add_argument(
+        '--methods',
+        type=str,
+        default='all',
+        help='Methods to use. Specify in comma-separated format -- "ERM,DA+ERM,DA+UIV,DA+IV". Default is "all".'
+    )
+    args = dict(vars(parser.parse_args()))
+    run(**args)
