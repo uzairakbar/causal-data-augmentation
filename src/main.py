2e5e4c22b9e4f76aa8b1afe305ca1ebe0e0a6321
Uzair Akbar <uzairakbar025@gmail.com>
2024-08-31 22:56:37 -0400

    Update configs

--- src/main.py
+++ src/main.py
@@ -21,16 +21,45 @@ if __name__ == '__main__':
     config.read('config.ini')
 
     logger.info('Running linear simulation experiment.')
-    linear_simulation.run(**config['LinearSimulation'])
+    linear_simulation.run(
+        seed = config.getint('linear simulation', 'seed'),
+        n_samples = config.getint('linear simulation', 'n_samples'),
+        x_dimension = config.getint('linear simulation', 'x_dimension'),
+        n_experiments = config.getint('linear simulation', 'n_experiments'),
+        sweep_samples = config.getint('linear simulation', 'sweep_samples'),
+        methods = config.get('linear simulation', 'methods')
+    )
 
     logger.info('Running non-linear simulation experiment.')
-    nonlinear_simulation.run(**config['NonlinearSimulation'])
+    nonlinear_simulation.run(
+        seed = config.getint('nonlinear simulation', 'seed'),
+        n_samples = config.getint('nonlinear simulation', 'n_samples'),
+        n_experiments = config.getint('nonlinear simulation', 'n_experiments'),
+        methods = config.get('nonlinear simulation', 'methods')
+    )
 
     logger.info('Running optical device experiment.')
-    optical_device_experiment.run(**config['OpticalDevice'])
+    optical_device_experiment.run(
+        seed = config.getint('optical device', 'seed'),
+        n_samples = config.getint('optical device', 'n_samples'),
+        x_dimension = config.getint('optical device', 'x_dimension'),
+        n_experiments = config.getint('optical device', 'n_experiments'),
+        sweep_samples = config.getint('optical device', 'sweep_samples'),
+        methods = config.get('optical device', 'methods')
+    )
 
     logger.info('Running colored MNIST experiment.')
-    cmnist_experiment.run(**config['ColoredMNIST'])
+    cmnist_experiment.run(
+        seed = config.getint('colored mnist', 'seed'),
+        num_seeds = config.getint('colored mnist', 'num_seeds'),
+        n_samples = config.getint('colored mnist', 'n_samples'),
+        methods = config.get('colored mnist', 'methods')
+    )
 
     logger.info('Running rotated MNIST experiment.')
-    rmnist_experiment.run(**config['RotatedMNIST'])
+    rmnist_experiment.run(
+        seed = config.getint('rotated mnist', 'seed'),
+        num_seeds = config.getint('rotated mnist', 'num_seeds'),
+        n_samples = config.getint('rotated mnist', 'n_samples'),
+        methods = config.get('rotated mnist', 'methods')
+    )
