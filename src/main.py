7753d6a7c69a0a1cfb185436e6a9a80d8029a86b
Uzair Akbar <uzairakbar025@gmail.com>
2024-09-04 01:17:04 -0400

    Change config to yaml

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import os
 import sys
-import configparser
+import yaml
 from loguru import logger
+from munch import munchify
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -11,52 +12,47 @@ from src.experiments.simulation import (
 )
 from src.experiments.real import (
     optical_device as optical_device_experiment,
-    cmnist as cmnist_experiment,
-    rmnist as rmnist_experiment,
+    cmnist as colored_mnist_experiment,
+    rmnist as rotated_mnist_experiment,
 )
 
 if __name__ == '__main__':
 
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+    with open('src/config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+    config = munchify(config)
 
-    logger.info('Running linear simulation experiment.')
-    linear_simulation.run(
-        seed = config.getint('linear simulation', 'seed'),
-        n_samples = config.getint('linear simulation', 'n_samples'),
-        x_dimension = config.getint('linear simulation', 'x_dimension'),
-        n_experiments = config.getint('linear simulation', 'n_experiments'),
-        sweep_samples = config.getint('linear simulation', 'sweep_samples'),
-        methods = config.get('linear simulation', 'methods')
-    )
-
-    logger.info('Running non-linear simulation experiment.')
-    nonlinear_simulation.run(
-        seed = config.getint('nonlinear simulation', 'seed'),
-        n_samples = config.getint('nonlinear simulation', 'n_samples'),
-        n_experiments = config.getint('nonlinear simulation', 'n_experiments'),
-        methods = config.get('nonlinear simulation', 'methods')
-    )
-
-    logger.info('Running optical device experiment.')
-    optical_device_experiment.run(
-        seed = config.getint('optical device', 'seed'),
-        n_samples = config.getint('optical device', 'n_samples'),
-        methods = config.get('optical device', 'methods')
-    )
-
-    logger.info('Running colored MNIST experiment.')
-    cmnist_experiment.run(
-        seed = config.getint('colored mnist', 'seed'),
-        num_seeds = config.getint('colored mnist', 'num_seeds'),
-        n_samples = config.getint('colored mnist', 'n_samples'),
-        methods = config.get('colored mnist', 'methods')
-    )
-
-    logger.info('Running rotated MNIST experiment.')
-    rmnist_experiment.run(
-        seed = config.getint('rotated mnist', 'seed'),
-        num_seeds = config.getint('rotated mnist', 'num_seeds'),
-        n_samples = config.getint('rotated mnist', 'n_samples'),
-        methods = config.get('rotated mnist', 'methods')
-    )
+    if config.linear_simulaton:
+        logger.info('Running linear simulation experiment.')
+        linear_simulation.run(
+            **config.linear_simulaton,
+            **config.hyperparameters
+        )
+    
+    if config.nonlinear_simulaton:
+        logger.info('Running non-linear simulation experiment.')
+        nonlinear_simulation.run(
+            **config.nonlinear_simulaton,
+            **config.hyperparameters
+        )
+    
+    if config.optical_device:
+        logger.info('Running optical device experiment.')
+        optical_device_experiment.run(
+            **config.optical_device,
+            **config.hyperparameters
+        )
+    
+    if config.colored_mnist:
+        logger.info('Running colored MNIST experiment.')
+        colored_mnist_experiment.run(
+            **config.colored_mnist,
+            **config.hyperparameters
+        )
+    
+    if config.rotated_mnist:
+        logger.info('Running rotated MNIST experiment.')
+        rotated_mnist_experiment.run(
+            **config.rotated_mnist,
+            **config.hyperparameters
+        )
