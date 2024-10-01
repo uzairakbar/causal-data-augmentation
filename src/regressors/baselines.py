eb4c68514ee0ed2938ef9273c7f334ad8317da88
Uzair Akbar <uzairakbar025@gmail.com>
2024-10-01 03:44:15 -0400

    Cap ICP subsets to prevent combinatorial explosion.

--- src/regressors/baselines.py
+++ src/regressors/baselines.py
@@ -1,14 +1,14 @@
 import torch
+import random
 import numpy as np
 from typing import List
 from loguru import logger
 from torch import autograd
 from abc import abstractmethod
 import torch.nn.functional as F
-from scipy.stats import ttest_ind
-from scipy.stats import f as fdist
 import torch.utils.data as data_utils
 from itertools import chain, combinations
+from scipy.stats import f as fdist, ttest_ind
 from sklearn.linear_model import LinearRegression
 
 from src.regressors.abstract import BaselineRegressor
@@ -19,6 +19,7 @@ from src.regressors.utils import MODELS, Model, device
 DEVICE: str=device()
 MAX_BATCH: int=2_500
 LOG_FREQUENCY: int=100
+MAX_ICP_SUBSETS: int=1024
 
 
 class LinearAnchorRegression(BaselineRegressor):
@@ -62,7 +63,8 @@ class InvariantCausalPrediction(BaselineRegressor):
         environments = np.unique(Z, axis=0)
 
         accepted_subsets = []
-        for subset in self._powerset(range(M)):
+        randomised_subsets = self._random_powerset(range(M))
+        for i, subset in enumerate(randomised_subsets):
             if len(subset) == 0:
                 continue
 
@@ -87,12 +89,15 @@ class InvariantCausalPrediction(BaselineRegressor):
 
                 p_values.append(self._mean_var_test(res_in, res_out))
             
-            # TODO: Jonas uses "min(p_values) * len(environments) - 1"
+            # Jonas uses `min(p_values) * len(environments) - 1`
             p_value = min(p_values) * len(environments)
 
             if p_value > self._alpha:
                 accepted_subsets.append(set(subset))
                 logger.info(f'Accepted subset: {subset}')
+            
+            if i >= MAX_ICP_SUBSETS:
+                break
         
         if len(accepted_subsets):
             accepted_features = list(set.intersection(*accepted_subsets))
@@ -121,10 +126,18 @@ class InvariantCausalPrediction(BaselineRegressor):
         pvalue_var2 = 2 * min(pvalue_var1, 1 - pvalue_var1)
 
         return 2 * min(pvalue_mean, pvalue_var2)
-
-    def _powerset(self, s: List[int]):
+    
+    def _random_powerset(self, s: List[int]):
+        '''
+        Since exhaustive search over all feature combinations can be expensive,
+        we randomise the powerset for reasonable exploration of feature combos 
+        and only try the first `MAX_ICP_SUBSETS` (non-singleton) subsets.
+        '''
+        s = list(s)
+        lengths = list(range(len(s)+1))
+        random.shuffle(lengths)
         return chain.from_iterable(
-            combinations(s, r) for r in range(len(s) + 1)
+            combinations(s, r) for r in lengths if not random.shuffle(s)
         )
 
 
