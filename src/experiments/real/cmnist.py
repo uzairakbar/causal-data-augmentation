Fix causal regularization + Add nCER

src/experiments/real/cmnist.py
@@ -1,9 +1,9 @@
 import enlighten
 import numpy as np
+import scipy as sp
 from loguru import logger
 from argparse import ArgumentParser
 from typing import Dict, Callable, Optional, List
-from sklearn.preprocessing import PolynomialFeatures
 
 from src.data_augmentors.real.cmnist import ColoredDigitsDA as DA
 
@@ -38,6 +38,7 @@ from src.experiments.utils import (
     tex_table,
     fit_model,
     estimation_error,
+    cmnist_estimation_error,
     ANNOTATE_BOX_PLOT,
 )
 
@@ -49,10 +50,6 @@ EXPERIMENT: str='colored_mnist'
 DEFAULT_CV_SAMPLES: int=10
 DEFAULT_CV_FRAC: float=0.2
 DEFAULT_CV_JOBS: int=1
-POLYNOMIAL_DEGREE: int=1
-FEATURES = PolynomialFeatures(
-    POLYNOMIAL_DEGREE, include_bias=False
-)
 
 
 def run(
@@ -179,7 +176,6 @@ def run(
 
             X, y = sem(N = n_samples)
             GX, G = da(X)
-            G = FEATURES.fit_transform(G)
             
             pbar_methods = MANAGER.counter(
                 total=len(methods), desc=f'Seed {seed+i}', unit='methods', leave=False
@@ -198,7 +194,7 @@ def run(
                 )
                 
                 ate_test_hat = model.predict(X_test)
-                error = estimation_error(
+                error = cmnist_estimation_error(
                     ate_test, ate_test_hat
                 )
                 all_errors[augmentation][method_name][i] = error
@@ -215,21 +211,10 @@ def run(
     save(
         obj=all_errors, fname=EXPERIMENT, experiment=EXPERIMENT, format='pkl'
     )
-    save(
-        obj=all_errors, fname=EXPERIMENT, experiment=EXPERIMENT, format='json'
-    )
-    save(
-        obj=np.arange(seed, seed+num_seeds),
-        fname='seeds', experiment=EXPERIMENT, format='json'
-    )
     
     box_plot(
-        all_errors, fname=EXPERIMENT+'_bootstrapped', experiment=EXPERIMENT,
-        savefig=True, **ANNOTATE_BOX_PLOT[EXPERIMENT]
-    )
-    box_plot(
         all_errors, fname=EXPERIMENT, experiment=EXPERIMENT,
-        savefig=True, **ANNOTATE_BOX_PLOT[EXPERIMENT], bootstrapped=False
+        savefig=True, **ANNOTATE_BOX_PLOT[EXPERIMENT]
     )
     
     caption = (
@@ -237,13 +222,6 @@ def run(
     )
     table = tex_table(
         all_errors, label=EXPERIMENT, caption=caption
-    )
-    save(
-        obj=table, fname=EXPERIMENT+'_bootstrapped', experiment=EXPERIMENT, format='tex'
-    )
-
-    table = tex_table(
-        all_errors, label=EXPERIMENT, caption=caption, bootstrapped=False
     )
     save(
         obj=table, fname=EXPERIMENT, experiment=EXPERIMENT, format='tex'

src/experiments/simulation/linear.py
@@ -200,7 +200,7 @@ class GammaSweep(SweepExperiment):
 
     def param_sweep(self):
         gamma_values = np.logspace(
-            -3, 1, base=10, num=self.sweep_samples
+            -2.5, 1, base=10, num=self.sweep_samples
         )
         return gamma_values
 
@@ -476,27 +476,14 @@ def run(
     save(
         obj=results, fname=EXPERIMENT, experiment=EXPERIMENT, format='pkl'
     )
-    save(
-        obj=results, fname=EXPERIMENT, experiment=EXPERIMENT, format='json'
-    )
-    box_plot(
-        results, fname=EXPERIMENT+'_bootstrapped', experiment=EXPERIMENT,
-        savefig=True, **ANNOTATE_BOX_PLOT[EXPERIMENT]
-    )
     box_plot(
         results, fname=EXPERIMENT, experiment=EXPERIMENT,
-        savefig=True, **ANNOTATE_BOX_PLOT[EXPERIMENT], bootstrapped=False
+        savefig=True, **ANNOTATE_BOX_PLOT[EXPERIMENT]
     )
     
     caption = f'nCER $\pm$ one std across {n_experiments} experiments of {n_samples} samples each.',
     table = tex_table(
         results, label=EXPERIMENT, caption=caption
-    )
-    save(
-        obj=table, fname=EXPERIMENT+'_bootstrapped', experiment=EXPERIMENT, format='tex'
-    )
-    table = tex_table(
-        results, label=EXPERIMENT, caption=caption, bootstrapped=False
     )
     save(
         obj=table, fname=EXPERIMENT, experiment=EXPERIMENT, format='tex'

src/regressors/baselines.py
@@ -6,6 +6,7 @@ from loguru import logger
 from torch import autograd
 from abc import abstractmethod
 import torch.nn.functional as F
+from sklearn.preprocessing import PolynomialFeatures
 import torch.utils.data as data_utils
 from itertools import chain, combinations
 from scipy.stats import f as fdist, ttest_ind
@@ -115,9 +116,7 @@ class InvariantCausalPrediction(BaselineRegressor):
         )
 
 
-class KaniaWit(BaselineRegressor):
-    # __init__, predict, get_params, set_params are all correct.
-    
+class KaniaWit(BaselineRegressor):    
     def _fit(self, X, y, **kwargs):
         if 'groups' in kwargs:
             groups = kwargs['groups']
@@ -327,7 +326,7 @@ class InvariantRiskMinimization(NonlinearBaselineRegressor):
                 )
             )
         
-        loss = torch.stack(env_losses).sum()
+        loss = torch.stack(env_losses).mean()
 
         return loss
     
@@ -336,7 +335,7 @@ class InvariantRiskMinimization(NonlinearBaselineRegressor):
 
         loss = R(phi(X) @ w, y)
         gradient = autograd.grad(loss, [w], create_graph=True)[0]
-        penalty = torch.sum(gradient**2)
+        penalty = torch.mean(gradient**2)
 
         loss = R(phi(X), y)
 
@@ -450,6 +449,7 @@ class RICE(NonlinearBaselineRegressor):
             self,
             X, y,
             da,
+            poly_degree: int=1,
             num_augmentations: int=3,
             lr: float=0.001, epochs: int=40, batch: int=128,
             pbar_manager=None,
@@ -457,18 +457,28 @@ class RICE(NonlinearBaselineRegressor):
         ):
         def flatten(x):
             return x.reshape(*x.shape[:1], -1)
-
+        
+        self.features = PolynomialFeatures(
+            poly_degree, include_bias=False
+        )
+        
         I_g = ([
             flatten(da(X)[0])
             for _ in range(num_augmentations)
         ])
         I_g = ([
-            torch.tensor(GX, dtype=torch.float32).to(DEVICE)
+            torch.tensor(
+                self.features.fit_transform(GX),
+                dtype=torch.float32
+            ).to(DEVICE)
             for GX in I_g
         ])
 
         X = flatten(X)
-        X = torch.tensor(X, dtype=torch.float).to(DEVICE)
+        X = torch.tensor(
+            self.features.fit_transform(X),
+            dtype=torch.float
+        ).to(DEVICE)
         N, M = X.shape
 
         self.f = self._model(M)
@@ -500,7 +510,6 @@ class RICE(NonlinearBaselineRegressor):
             if batch_mode == 'full':
                 self.fit_f_batch(X, y, I_g)
             else:
-                # logger.info(f'g epoch {epoch + 1}/{epochs}')
                 self.fit_f_minibatch(train)
             
             if pbar_manager: pbar_epochs.update()
@@ -538,3 +547,8 @@ class RICE(NonlinearBaselineRegressor):
         else:
             loss = F.mse_loss(y_hat, y, reduction=reduction)
         return loss
+    
+    def _predict(self, X, **kwargs):
+        return super()._predict(
+            self.features.fit_transform(X)
+        )